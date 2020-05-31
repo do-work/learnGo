9236a4b73f2ad2a0032552d009d78f570c05b43d
David Olson <david@deolson.com>
2020-05-31 19:02:11 -0400

    ch7-maps: sorting a maps values.

--- status.go
+++ status.go
@@ -6,15 +6,13 @@ import (
 )
 
 func status() {
-	grades := map[string]float64{"Alma": 0, "Rohit": 86.5}
+	grades := map[string]float64{"Alma": 0, "Rohit": 86.5, "Bebe": 30}
 	var names []string
 
 	for name := range grades {
 		names = append(names, name)
 	}
-
 	sort.Strings(names)
-
 	for _, name := range names {
 		fmt.Printf("%s has a grade of %0.1f%%\n", name, grades[name])
 	}
