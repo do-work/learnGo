02e99f9bc2f2af902287f99066015546d7d59430
David Olson <david@deolson.com>
2020-09-22 18:12:16 -0400

    ch15-http: Add handler, specify 2 routes and run http server.

--- hello.go
+++ hello.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 )
 
-func main() {
-	fileInfo, err := os.Stat("my.txt")
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(fileInfo.Size())
+}
+
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Salut web!")
+}
+
+func main() {
+	http.HandleFunc("/hello", englishHandler)
+	http.HandleFunc("/salut", frenchHandler)
+
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
