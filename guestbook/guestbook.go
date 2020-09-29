c02f526a5032d4597e7cc8d902f24f5be51d3f00
David Olson <david@deolson.com>
2020-09-29 18:52:19 -0400

    ch16-html_templates: Create template, add route to write out template.

---
+++ guestbook/guestbook.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getStrings(filename string) []string {
+	var lines []string
+	file, err := os.Open(filename)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	fmt.Printf("%#v\n", signatures)
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	err = html.Execute(writer, signatures)
+	check(err)
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
